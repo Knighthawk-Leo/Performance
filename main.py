2373985434d49b361c739c527ad3f8ab4b0daa04
Sanskar Dwivedi <72437494+Knighthawk-Leo@users.noreply.github.com>
2023-05-23 02:12:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 from fastapi.responses import JSONResponse
-
+import json
 app = FastAPI()
 
 df = pd.read_csv('May2023StatementRun_EMUQTECHInc-royalty_product_and_asset.csv')
@@ -10,16 +10,18 @@ async def get_stream(Name):
     print(Name)
     streams = df[df['Asset Artist'] == Name].groupby(['DSP'])['Asset Quantity'].sum().sort_values(ascending=False)
     json_response = streams.reset_index().to_json(orient='records')
+    json_data = json.loads(json_response)
     
-    return JSONResponse(content=json_response)
+    return JSONResponse(content=json_data)
 
 @app.get("/performance/trends/{Name}")
 async def get_trends(Name):
     print(Name)
     trends= df[df['Asset Artist'] == Name].groupby(['Sale Start date'])['Asset Quantity'].sum()
     json_response = trends.reset_index().to_json(orient='records')
+    json_data = json.loads(json_response)
     
-    return JSONResponse(content=json_response)
+    return JSONResponse(content=json_data)
 
 
 
@@ -27,9 +29,10 @@ async def get_trends(Name):
 async def get_mapName(Name):
     print(Name)
     map= df[df['Asset Artist'] == "Adam Faith"].groupby(['Territory'])['Asset Quantity'].sum()
-    json_response =map.reset_index().to_json(orient='records')
+    json_response = map.reset_index().to_json(orient='records')
+    json_data = json.loads(json_response)
     
-    return JSONResponse(content=json_response)
+    return JSONResponse(content=json_data)
 
 @app.post("/")
 async def post():
